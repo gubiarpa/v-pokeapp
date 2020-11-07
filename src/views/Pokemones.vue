39a04bb60b00f15f55eb7db19f6728b856ea756c
gubiarpa <billy.arredondo@pucp.pe>
2020-11-07 03:19:10 -0500

    Get Pokemon List

--- src/views/Pokemones.vue
+++ src/views/Pokemones.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <Titulo texto="Lista de Pokemones" />
+    <div v-for="item in pokemonList" :key="item">
+      {{ item.name }}
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,25 @@ export default {
     name: 'Pokemones',
     components: {
         Titulo
+    },
+    data() {
+      return {
+        pokemonList: []
+      }
+    },
+    methods: {
+      async getPokemonList() {
+        try {
+          const data = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151'); // traemos de la API
+          const obj = await data.json(); // configuramos
+          this.pokemonList = obj.results;
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+    created() {
+      this.getPokemonList();
     }
 }
 </script>
