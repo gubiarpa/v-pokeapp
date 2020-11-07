0f293e05edf6159f9b94f8508dfa9e1981d84813
gubiarpa <billy.arredondo@pucp.pe>
2020-11-06 23:56:01 -0500

    Pokemones View Added
    List of pokemons

---
+++ src/components/Titulo.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{texto}}</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Pokemones.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Titulo texto="Lista de Pokemones" />
+  </div>
+</template>
+
+<script>
+import Titulo from "../components/Titulo";
+export default {
+    name: 'Pokemones',
+    components: {
+        Titulo
+    }
+}
+</script>
+
+<style>
+
+</style>
